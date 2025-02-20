b63d554b1cf19fb25233376e3481bed4d82d01b9
Abhi-engg <abhijeetyadav33xb@gmail.com>
2025-02-20 21:32:13 +0530

    hero page

--- Timetable/src/pages/dashboard/Settings.jsx
+++ Timetable/src/pages/dashboard/Settings.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import Button from '../../components/common/Button';
 import Card from '../../components/common/Card';
